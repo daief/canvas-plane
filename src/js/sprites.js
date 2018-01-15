b6af42459dd222152b91762722b4bae71aca86ee
DeFeNG <1437931235@qq.com>
2018-01-15 13:59:15 +0800

    仿写sprites

---
+++ src/js/sprites.js
@@ -0,0 +1,143 @@
+/**
+ * 图像绘制器
+ */
+class ImagePainter {
+  constructor(imageUrl) {
+    this.image = new Image()
+    this.image.src = imageUrl
+  }
+
+  paint(sprite, context) {
+    if (this.image !== undefined) {
+      if (!this.image.complete) {
+        this.image.onload = (e) => {
+          sprite.width = this.width
+          sprite.height = this.height
+
+          context.drawImage(e.target,
+            sprite.left, sprite.top,
+            sprite.width, sprite.height)
+        }
+      } else {
+        context.drawImage(this.image,
+          sprite.left, sprite.top,
+          sprite.width, sprite.height)
+      }
+    }
+  }
+}
+
+/**
+ * 精灵绘制器
+ */
+class SpriteSheetPainter {
+  constructor(cells, spritesheet) {
+    this.cells = cells
+    this.spritesheet = spritesheet
+    this.cellIndex = 0
+  }
+
+  advance() {
+    if (this.cellIndex == this.cells.length - 1) {
+      this.cellIndex = 0
+    } else {
+      this.cellIndex++
+    }
+  }
+
+  paint(sprite, context) {
+    let cell = this.cells[this.cellIndex]
+    context.drawImage(this.spritesheet,
+      cell.left, cell.top,
+      cell.width, cell.height,
+      sprite.left, sprite.top,
+      cell.width, cell.height);
+  }
+}
+
+/**
+ * 精灵动画制作器
+ * SpriteAnimator 包含两个参数，Painter 数组和回调函数。
+ */
+class SpriteAnimator {
+  constructor(painters, elapsedCallback) {
+    this.painters = painters
+    this.elapsedCallback = elapsedCallback || function () { }
+    
+    this.duration = 1000
+    this.startTime = 0
+    this.index = 0
+  }
+
+  end(sprite, originalPainter) {
+    sprite.animating = false
+
+    if (this.elapsedCallback) {
+      this.elapsedCallback(sprite)
+    } else {
+      sprite.painter = originalPainter
+    }
+  }
+
+  start(sprite, duration) {
+    let endTime = + new Date() + duration,
+      period = duration / (this.painters.length),
+      interval = null,
+      originalPainter = sprite.painter
+    
+    this.index = 0
+    sprite.animating = true
+    sprite.painter = this.painters[this.index]
+
+    interval = setInterval(() => {
+      if (+new Date() < endTime) {
+        sprite.painter = this.painters[++this.index];
+      }
+      else {
+        this.end(sprite, originalPainter);
+        clearInterval(interval);
+      }
+    }, period)
+  }
+}
+
+/**
+ * 精灵
+ */
+class Sprite {
+  constructor(name, painter, behaviors) {
+    this.name = name
+    this.painter = painter
+    this.behaviors = behaviors
+
+    this.left = 0
+    this.top = 0
+    this.width = 10
+    this.height = 10
+    this.velocityX = 0
+    this.velocityY = 0
+    this.visible = true
+    this.animating = false
+    this.painter = null
+    this.behaviors = []
+  }
+
+  paint(context) {
+    if (this.painter && this.visible) {
+      this.painter.paint(this, context)
+    }
+  }
+
+  update(context, time) {
+    for (let index = this.behaviors.length; index > 0; index--) {
+      this.behaviors[i - 1].execute(this, context, time)
+    }
+  }
+}
+
+export {
+  ImagePainter,
+  SpriteSheetPainter,
+  SpriteAnimator,
+  Sprite
+}
