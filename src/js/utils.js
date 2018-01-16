67491509def784c20a2e16edeee8026c22692e04
DeFeNG <1437931235@qq.com>
2018-01-16 20:52:31 +0800

    add guid method

--- src/js/utils.js
+++ src/js/utils.js
@@ -5,6 +5,22 @@ function getTimeNow() {
   return (+ new Date())
 }
 
+function getGUID() {
+  // https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript/8809472#8809472
+  let time = (new Date()).getTime()
+  if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
+    // 使用更高精度的时间
+    time += performance.now()
+  }
+  let guid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(char) {
+      var rand = (time + (Math.random() * 16)) % 16 | 0;
+      time = Math.floor(time / 16);
+      return (char === 'x' ? rand : ((rand & 0x3) | 0x8)).toString(16);
+  })
+  return guid
+}
+
 export {
-  getTimeNow
+  getTimeNow,
+  getGUID
 }
